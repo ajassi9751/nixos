705a3685f29906f2d10dd49cff5e25ce93e72d3c
Aikam Jassi <ajassi9751@gmail.com>
2026-04-08 17:29:05 -0400

    Added a commented shell option

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aikam = {
+    # shell = "${pkgs.zsh}/bin/zsh";
     isNormalUser = true;
     description = "Aikam";
     extraGroups = [ "networkmanager" "wheel" ];
