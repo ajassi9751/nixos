39292563856814f5b7c8ef3a9b601dbf74d3cec3
Aikam Jassi <ajassi9751@gmail.com>
2026-06-10 16:02:53 -0400

    Switched to kde

--- configuration.nix
+++ configuration.nix
@@ -58,8 +58,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   xdg.portal = {
     enable = true;
