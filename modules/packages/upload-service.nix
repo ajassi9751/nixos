869edc1a89f031fb063fffb49f20b548ad309667
ajassi9751 <ajassi9751@gmail.com>
2026-08-05 20:09:04 -0400

    Fixed syntax errors and formatted

--- modules/packages/upload-service.nix
+++ modules/packages/upload-service.nix
@@ -2,7 +2,7 @@
   perSystem =
     { pkgs, ... }:
     let
-      upload-service-program = pkgs.callPackage ./upload-service
+      upload-service-program = pkgs.callPackage ./upload-service { };
     in
     {
       packages.upload-service = upload-service-program;
--- modules/systems/server.nix
+++ modules/systems/server.nix
@@ -1,113 +1,114 @@
 { ... }: {
-  flake.nixosModules.server-system = { pkgs, ... }:
-  {
-    services.headscale = {
-      enable = true;
-      port = 8080;
+  flake.nixosModules.server-system =
+    { pkgs, ... }:
+    {
+      services.headscale = {
+        enable = true;
+        port = 8080;
 
-      settings = {
-        # The publicly accessible URL where your clients will reach Headscale
-        server_url = "https://colrec.ddns.me";
+        settings = {
+          # The publicly accessible URL where your clients will reach Headscale
+          server_url = "https://colrec.ddns.me";
 
-        # The port Headscale listens on internally
-        listen_addr = "0.0.0.0:8080";
+          # The port Headscale listens on internally
+          listen_addr = "0.0.0.0:8080";
 
-        # Tailscale IP prefix allocations for your VPN clients
-        ip_prefixes = [
-          "100.64.0.0/10"
-          "fd7a:115c:a1e0::/48"
-        ];
-
-        # DNS settings for connected nodes
-        dns = {
-          magic_dns = true;
-          base_domain = "hs.net";
-          nameservers.global = [
-            "1.1.1.1"
-            "9.9.9.9"
-            "8.8.8.8"
+          # Tailscale IP prefix allocations for your VPN clients
+          ip_prefixes = [
+            "100.64.0.0/10"
+            "fd7a:115c:a1e0::/48"
           ];
-        };
-      };
-    };
-    services.caddy = {
-      enable = true;
-      virtualHosts."colrec.ddns.me".extraConfig = ''
-        reverse_proxy localhost:8080
-      '';
-    };
-    # Open HTTP (80) and HTTPS (443) ports for ACME certificates
-    networking = {
-      networkmanager.enable = false;
-      firewall.allowedTCPPorts = [
-        80
-        443
-        8096
-        8920
-        8020
-      ];
-      firewall.allowedUDPPorts = [ 41641 ]; # Tailscale direct communication port
-      # Set DNS servers
-      nameservers = [
-        "1.1.1.1"
-        "9.9.9.9"
-        "8.8.8.8"
-      ];
-      # Set default gateway (my router's internal IP address)
-      defaultGateway = "10.8.88.254";
-      # Configure the interface with a static IP
-      interfaces.enp3s0 = {
-        ipv4.addresses = [
-          {
-            address = "10.8.88.10"; # Your chosen static IP
-            prefixLength = 24; # Subnet mask 255.255.255.0 is /24
-          }
-        ];
-      };
-    };
-    # services.nextcloud = {
-    #     enable = true;
-    #     hostName = "aikamcloud";
-    # };
-    services.jellyfin.enable = true;
-    # Auto backup
-    services.btrbk.instances."prot-backup" = {
-      onCalendar = "02:00";
 
-      settings = {
-        snapshot_preserve_min = "7d";
-        snapshot_preserve = "4w";
-
-        target_preserve_min = "7d";
-        target_preserve = "4w 6m";
-
-        volume."/int/prot" = {
-          snapshot_dir = ".snapshots";
-
-          subvolume."." = {
-            target = "/mnt/backup/prot_backup";
+          # DNS settings for connected nodes
+          dns = {
+            magic_dns = true;
+            base_domain = "hs.net";
+            nameservers.global = [
+              "1.1.1.1"
+              "9.9.9.9"
+              "8.8.8.8"
+            ];
           };
         };
       };
-    };
-    systemd.services.upload-service = {
-      description = "Upload service";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
+      services.caddy = {
+        enable = true;
+        virtualHosts."colrec.ddns.me".extraConfig = ''
+          reverse_proxy localhost:8080
+        '';
+      };
+      # Open HTTP (80) and HTTPS (443) ports for ACME certificates
+      networking = {
+        networkmanager.enable = false;
+        firewall.allowedTCPPorts = [
+          80
+          443
+          8096
+          8920
+          8020
+        ];
+        firewall.allowedUDPPorts = [ 41641 ]; # Tailscale direct communication port
+        # Set DNS servers
+        nameservers = [
+          "1.1.1.1"
+          "9.9.9.9"
+          "8.8.8.8"
+        ];
+        # Set default gateway (my router's internal IP address)
+        defaultGateway = "10.8.88.254";
+        # Configure the interface with a static IP
+        interfaces.enp3s0 = {
+          ipv4.addresses = [
+            {
+              address = "10.8.88.10"; # Your chosen static IP
+              prefixLength = 24; # Subnet mask 255.255.255.0 is /24
+            }
+          ];
+        };
+      };
+      # services.nextcloud = {
+      #     enable = true;
+      #     hostName = "aikamcloud";
+      # };
+      services.jellyfin.enable = true;
+      # Auto backup
+      services.btrbk.instances."prot-backup" = {
+        onCalendar = "02:00";
 
-      serviceConfig = {
-        Type = "simple";
-        User = "aikam";
-        Group = "users";
+        settings = {
+          snapshot_preserve_min = "7d";
+          snapshot_preserve = "4w";
 
-        ExecStart = "${self.packages.upload-service}/bin/upload-service /int/prot/jellyfin/family-media";
+          target_preserve_min = "7d";
+          target_preserve = "4w 6m";
 
-        WorkingDirectory = "/etc/nixos/modules/packages/upload-service";
+          volume."/int/prot" = {
+            snapshot_dir = ".snapshots";
 
-        # Hardening properties
-        Restart = "on-failure";
-        PrivateTmp = true;
+            subvolume."." = {
+              target = "/mnt/backup/prot_backup";
+            };
+          };
+        };
+      };
+      systemd.services.upload-service = {
+        description = "Upload service";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          User = "aikam";
+          Group = "users";
+
+          ExecStart = "${self.packages.upload-service}/bin/upload-service /int/prot/jellyfin/family-media";
+
+          WorkingDirectory = "/etc/nixos/modules/packages/upload-service";
+
+          # Hardening properties
+          Restart = "on-failure";
+          PrivateTmp = true;
+        };
       };
     };
-  };
 }
