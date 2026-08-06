9e286c87d937ceecc5ff544a04866daf7d713e9f
ajassi9751 <ajassi9751@gmail.com>
2026-08-05 20:20:39 -0400

    Same thing again

--- modules/packages/upload-service.nix
+++ modules/packages/upload-service.nix
@@ -2,7 +2,12 @@
   perSystem =
     { pkgs, ... }:
     let
-      upload-service-program = ./upload-service/default.nix { inherit pkgs; };
+      upload-service-program = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "upload-service";
+        version = "0.1.0";
+        src = ./upload-service/.;
+        cargoLock.lockFile = ./upload-service/Cargo.lock;
+      };
     in
     {
       packages.upload-service = upload-service-program;
--- modules/packages/upload-service/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "upload-service";
-  version = "0.1.0";
-  src = ./.;
-  cargoLock.lockFile = ./Cargo.lock;
-}
