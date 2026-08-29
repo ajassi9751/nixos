321f013180631edf062832162214b4a61c9902b7
Aikam Jassi <ajassi9751@gmail.com>
2026-08-29 16:25:30 -0400

    Fix sup making root take ownership of .git when updating lock files

--- modules/packages/update-utils.nix
+++ modules/packages/update-utils.nix
@@ -22,13 +22,13 @@
         	                        # If you just want to update
         	                        if [[ -z "$1" ]]; then
         	                        	cd /home/aikam/nixos
-        	                        	nix flake update nixpkgs-unstable nixpkgs --commit-lock-file
+        	                        	sudo -u aikam nix flake update nixpkgs-unstable nixpkgs --commit-lock-file
         	                        	nixos-rebuild switch $FLAKE_FLAG
         	                        	exit 0
         	                        # Create a boot entry instead of switching
         	                        elif [[ "$1" == "boot" ]]; then
         	                        	cd /home/aikam/nixos
-        	                        	nix flake update nixpkgs-unstable nixpkgs --commit-lock-file
+        	                        	sudo -u aikam nix flake update nixpkgs-unstable nixpkgs --commit-lock-file
         	                        	nixos-rebuild boot $FLAKE_FLAG
         	                        	exit 0
         	                        # Stands for no update, doesn't update the packages, just switches
